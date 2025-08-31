6434a2633c112e0f5f39431450c37224d19bd505
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2025-08-31 21:51:05 +0700

    Upload project Hello World

---
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.helloworldapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello World");
+        tv.setTextSize(24);
+        setContentView(tv);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
